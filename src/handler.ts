d00034bbd58f8cd831ca2f05c35397cdcff18b0c
COD3VIL <73862378+michaelnji@users.noreply.github.com>
2025-06-06 21:47:26 +0100

    refactor: 🎨 enhance ServerResponseError class and improve sendServerResponse function validation

--- src/handler.ts
+++ src/handler.ts
@@ -1,34 +1,61 @@
 import type { ErrorCodes, ServerResponse, StatusCodes } from "./types";
+import { errorCodes, successCodes } from "./utils/defaults";
+
 
 /**
  * Sends a server response based on the provided status code, message, and optional data.
  * 
- * @template S - The type of status code, which can be either an ErrorCode or a StatusCode.
- * @template D - The type of data to be included in the response, applicable for success responses.
+ * @param status - The HTTP status code for the response. It can be either a success or error code.
+ * @param message - A descriptive message about the response.
+ * @param data - Optional data to include in the response. This is only included if the status code is a success code.
  * 
- * @param {S} status - The HTTP status code for the response.
- * @param {string} message - The message to be included in the response.
- * @param {D} [data] - Optional data to be included in the response, applicable for success responses.
+ * @returns A ServerResponse object containing the status, message, ok flag, and optional data.
  * 
- * @returns {ServerResponse<S, D>} - The server response object containing status, message, ok flag, and optional data.
+ * @throws Will throw an error if the status code is not recognized as a valid success or error code.
  */
 export function sendServerResponse<S extends ErrorCodes>(
     status: S,
     message: string
 ): ServerResponse<S, unknown>;
 
+/**
+ * Sends a server response based on the provided status code, message, and data.
+ * 
+ * @param status - The HTTP status code for the response. It must be a success code.
+ * @param message - A descriptive message about the response.
+ * @param data - Data to include in the response.
+ * 
+ * @returns A ServerResponse object containing the status, message, ok flag, and data.
+ * 
+ * @throws Will throw an error if the status code is not recognized as a valid success code.
+ */
 export function sendServerResponse<S extends StatusCodes, D>(
     status: S,
     message: string,
     data: D
 ): ServerResponse<S, D>;
 
+/**
+ * Sends a server response based on the provided status code, message, and optional data.
+ * 
+ * @param status - The HTTP status code for the response. It can be either a success or error code.
+ * @param message - A descriptive message about the response.
+ * @param data - Optional data to include in the response. This is only included if the status code is a success code.
+ * 
+ * @returns A ServerResponse object containing the status, message, ok flag, and optional data.
+ * 
+ * @throws Will throw an error if the status code is not recognized as a valid success or error code.
+ */
 export function sendServerResponse(
     status: StatusCodes | ErrorCodes,
     message: string,
     data: unknown = null
 ): ServerResponse<StatusCodes | ErrorCodes, unknown> {
-    const isSuccess = status === 200;
+    if (successCodes.find((x) => x === status) === undefined && errorCodes.find((x) => x === status) === undefined) {
+        throw new Error('incorrect value for status')
+    }
+    const isSuccess = successCodes.find((x) => x === status) !== undefined
+
     return {
         status,
         message,
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+// export const IsSuccess = Symbol('success-response')
 export type StatusCodes = 100 | 101 | 102 | 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 226 | 300 | 301 | 302 | 303 | 304 | 305 | 306 | 307 | 308;
 export type ErrorCodes = 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | 421 | 422 | 423 | 424 | 425 | 426 | 428 | 429 | 431 | 451 | 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 508 | 510 | 511;
 export interface ServerResponse<
@@ -6,10 +7,10 @@ export interface ServerResponse<
 > {
     status: Status;
     message?: string;
-    ok: Status extends 200 ? true : false;
-    data?: Status extends 200 ? Data : null;
+    ok: Status extends StatusCodes ? true : false;
+    data?: Status extends StatusCodes ? Data : null;
+    // [IsSuccess]?: 'success-response'
 }
-
 export interface NexusError extends Error {
     code: ErrorCodes;
     message: string;
